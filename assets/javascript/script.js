09fde336cbac9d136ab3b61d8cc0f88e8bf12ca2
Ckopei <cameron@kopels.com>
2019-09-17 20:44:09 -0400

    psuedocode for the javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,3 +28,26 @@ var firebaseConfig = {
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+var database = firebase.database()
+
+//user form
+
+//on click for add train button
+//set vars for each form
+//put vars into temporary object
+//push object to database
+//clear every text box on the page with .val("")
+
+
+//database side
+
+//on child added so it calls this every time an entry is made
+//database.ref().on("child_added", function(childSnapshot) {}
+// set variables from the snapshot 
+//console log it so I know it worked
+//append the snapshot variables to the page
+//var for <tr>
+//append all the <tr>'s to it.
+//append new <tr> to the tbody
+
+//cry as you figure out how to work in the moment.js math
